84e0bd4288b693acb05f947a8f2bf7684b56f2bc
Luan Pinto <luan.nelmo@hotmail.com>
2022-09-15 20:31:41 -0300

    Api Bootcamp Spring

---
+++ src/main/java/dio/quebec/api/ApiSpringApplication.java
@@ -0,0 +1,13 @@
+package dio.quebec.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/quebec/api/ApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.quebec.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
